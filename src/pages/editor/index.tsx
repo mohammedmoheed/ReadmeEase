73f0579b634adafa709f971257517ee232db170b
Harshdeep Singh <harsh.code97@gmail.com>
2023-02-12 12:39:45 +0530

    Created 'editor' page

---
+++ src/pages/editor/index.tsx
@@ -0,0 +1,15 @@
+import PageHeader from "@/components/Page/Header";
+
+export default function Home() {
+  return (
+    <>
+      <PageHeader
+        title="Editor | ReadmeEase"
+        description="Discover the power of effortless README creation with the editor feature of ReadmeEase. Create professional-looking documentation with ease using its drag and drop interface and dynamic customization options, all built with Next.js and Tailwind CSS."
+      />
+      <main>
+        <h1>Editor</h1>
+      </main>
+    </>
+  );
+}
