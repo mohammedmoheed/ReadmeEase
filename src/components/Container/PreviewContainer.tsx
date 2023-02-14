4db70747ab7f97c8922327f31e1e69c7a5e6bb97
Harshdeep Singh <harsh.code97@gmail.com>
2023-02-15 01:00:38 +0530

    Fix: Change preview background color

--- src/components/Container/PreviewContainer.tsx
+++ src/components/Container/PreviewContainer.tsx
@@ -5,7 +5,7 @@ function PreviewContainer({ body }: { body: string }): JSX.Element {
   return (
     <ReactMarkdown
       remarkPlugins={[remarkGfm]}
-      className="preview p-4 min-h-[calc(100vh-151px)] max-h-[calc(100vh-151px)] h-full w-full bg-[#262626] overflow-y-auto"
+      className="preview p-4 min-h-[calc(100vh-151px)] max-h-[calc(100vh-151px)] h-full w-full bg-[#0d1117] overflow-y-auto"
     >
       {body}
     </ReactMarkdown>
