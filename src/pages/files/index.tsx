d9f70f61f61a4e126551f802c18c2e5a994dfd36
Harshdeep Singh <harsh.code97@gmail.com>
2023-02-15 12:53:33 +0530

    Fix: Update 'files' page's title and description

--- src/pages/files/index.tsx
+++ src/pages/files/index.tsx
@@ -15,8 +15,8 @@ export default function FilePage() {
   return (
     <div className={poppins.className}>
       <PageHeader
-        title="Editor | ReadmeEase"
-        description="Discover the power of effortless README creation with the editor feature of ReadmeEase. Create professional-looking documentation with ease using its drag and drop interface and dynamic customization options, all built with Next.js and Tailwind CSS."
+        title="Files | ReadmeEase"
+        description="The 'Files' page of ReadmeEase is where you can view, manage, and organize your files in a user-friendly way. With ReadmeEase, managing your files has never been easier. Keep your files organized and accessible with our intuitive and efficient file management system. Try ReadmeEase today and simplify your file management tasks!"
       />
       <Header />
       <main className="relative w-full h-auto">
